bb210e9809ec458d3b49fcfbd7e74dd3ff855a22
Abhipsa <70021736+rivieraviews@users.noreply.github.com>
2025-08-09 14:05:35 +0530

    refactor(ui): change text color for app name

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
     
     return (
         <header className="flex items-center justify-between px-6 py-4 shadow bg-white">
-            <h1 className="text-2xl font-bold text-gray-900">Credible: A CC Management System</h1>
+            <h1 className="text-2xl font-bold text-gray-800">Credible: A CC Management System</h1>
             <div className="flex gap-4 items-center">
                 <Link to="/add-card">
                     <button className="px-4 py-2 rounded bg-primary text-white">+ Add Card</button>
